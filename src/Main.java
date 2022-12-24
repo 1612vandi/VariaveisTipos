a36c3657a7774eca736934c5353bff36d4b686c3
vandi1612 <vandidanielmartinsfrancisco@gmail.com>
2022-12-24 09:22:52 +0000

    Variaveis Primitivas

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int cep = 21334465;
+        double salario = 2500;
+        float pi = 3.12F;
+        short ano = 2022;
+        long cpf = 2345346356L;
+        byte idade = 21;
+
+        System.out.println("CEP\n" + cep + "Salario\n" + salario + "PI\n" + pi + "Ano\n" + ano + "CPF\n" + cpf + "idade\n" + idade);
+    }
+}
